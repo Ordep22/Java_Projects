70d94a1a1735689fdabfe78df7902b1ef824c32e
Ordep22 <pedrovitorcomercial@gmail.com>
2022-09-23 21:39:48 -0300

    Initial commit

---
+++ Aula_03/Aula_03.java
@@ -0,0 +1,59 @@
+package Aula_03;
+public class Aula_03{
+
+    public static void main (String [] args){
+
+        String nome_01;
+
+        nome_01 = "Pedro";
+
+        System.out.println("Olá Dev's!");
+        System.out.println("Meu nome é "+nome_01+ ".");
+        System.out.println("Hoje vamos estudar sober as variáveis primitivas.");
+        System.out.println("Aula_03: Tipos de dados primitivos");
+        System.out.println("Let's code");
+
+        
+        //////////////////////////////////
+        int num_01, num_02, res;
+
+        num_01 = 1;
+        num_02 = 5;
+
+
+        res = num_01 + num_02;
+
+        System.out.println("Soma de dois números inteiros");
+        System.out.println(num_01+" + "+num_02+" = "+res);
+
+
+
+
+
+        
+
+
+
+
+        
+
+
+        // Os dados básicos da programacão são:
+        // Int 
+        // Short 
+        // Long 
+        // String
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
